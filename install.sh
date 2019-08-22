77582c4739e57616bd1e4e6d8e6c94175e92093a
cry0this <cry0this@gmail.com>
2019-08-22 14:25:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ DIR=`cd $(dirname "${BASH_SOURCE[0]}"); pwd`
 cp $DIR/zram.conf /etc/zram.conf
 cp $DIR/zramctl /usr/bin/zramctl
 cp $DIR/zram.service /lib/systemd/system/zram.service
-chmod +x /usr/bin/zramctl
+chmod +x /usr/local/bin/zramctl
 systemctl daemon-reload
 systemctl enable zram.service
 systemctl start zram.service
