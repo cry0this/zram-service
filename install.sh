19eb9eae02df97d0a8cfa8edab23e81dfc144be7
alex <>
2022-10-19 13:45:39 +0300

    change systemd unit directory

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 DIR=`cd $(dirname "${BASH_SOURCE[0]}"); pwd`
 cp $DIR/zram.conf /etc/zram.conf
 cp $DIR/zramctl /usr/local/bin/zramctl
-cp $DIR/zram.service /lib/systemd/system/zram.service
+cp $DIR/zram.service /etc/systemd/system/zram.service
 chmod +x /usr/local/bin/zramctl
 systemctl daemon-reload
 systemctl enable zram.service
