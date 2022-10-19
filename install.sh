4840623ea7e55e808bb63c12d97b2e79f961425d
alex <>
2022-10-19 13:53:51 +0300

    rename zramctl to zram-service-ctl

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=`cd $(dirname "${BASH_SOURCE[0]}"); pwd`
 cp $DIR/zram.conf /etc/zram.conf
-cp $DIR/zramctl /usr/local/bin/zramctl
+cp $DIR/zram-service-ctl /usr/local/bin/zram-service-ctl
 cp $DIR/zram.service /etc/systemd/system/zram.service
 chmod +x /usr/local/bin/zramctl
 systemctl daemon-reload
